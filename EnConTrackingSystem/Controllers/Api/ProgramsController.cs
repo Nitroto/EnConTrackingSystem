46dcd9e083f143874ef32aec8165777aae4cdb09
Asen Todorov <astodorov@outlook.com>
2017-02-10 17:43:04 +0200

    programs view and controler started

---
+++ EnConTrackingSystem/Controllers/Api/ProgramsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using EnConTrackingSystem.Models;
+
+namespace EnConTrackingSystem.Controllers.Api
+{
+    public class ProgramsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public ProgramsController()
+        {
+            this._context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<Program> GetPrograms(string query = null)
+        {
+            var programsQuery = this._context.Programs;
+
+            if (!String.IsNullOrWhiteSpace(query))
+            {
+                //programsQuery = programsQuery.Where(p => p.Name.Contains(query));
+            }
+
+            return programsQuery.ToList();
+        }
+    }
+}
--- EnConTrackingSystem/Controllers/ProgramsController.cs
+++ EnConTrackingSystem/Controllers/ProgramsController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EnConTrackingSystem.Models;
 
 namespace EnConTrackingSystem.Controllers
 {
     public class ProgramsController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public ProgramsController()
+        {
+            this._context = new ApplicationDbContext();
+        }
+
         // GET: Programs
         public ActionResult Index()
         {
+            return View("List");
+        }
+
+        public ViewResult New()
+        {
             return View();
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            this._context.Dispose();
         }
     }
 }
